0cccbd1994a9fa665ddfc26889bfa8ac36f3736e
Javier Celaya <javier.celaya@flexvdi.com>
2016-12-14 10:32:14 +0100

    Add appimagetool to docker images

--- build-images.sh
+++ build-images.sh
@@ -5,8 +5,10 @@ set -e
 
 docker build -t linux-portable-x86_64 docker
 
-rm -f docker/Dockerfile.i686
-cp docker/Dockerfile docker/Dockerfile.i686
-sed -i -e "s/:6-x86_64/:6-i386/" -e "s/libc6-dev-i386/libc6-dev-amd64/" -e "s/Architecture.X86_64/Architecture.X86/" docker/Dockerfile.i686
+sed -e "s/:6-x86_64/:6-i386/" \
+    -e "s/libc6-dev-i386/libc6-dev-amd64/" \
+    -e "s/Architecture.X86_64/Architecture.X86/" \
+    -e "s/appimagetool-x86_64/appimagetool-i686/" \
+    docker/Dockerfile > docker/Dockerfile.i686
 docker build -t linux-portable-i686 -f docker/Dockerfile.i686 docker
 
