3ad60edf0b8a0c2205595ee27285bfe0a8974b65
Javier Celaya <javier.celaya@flexvdi.com>
2016-12-21 11:00:25 +0100

    Force build architecture to avoid mistakes

--- build-images.sh
+++ build-images.sh
@@ -9,6 +9,7 @@ docker build -t linux-portable-x86_64 $BUILD_ARGS docker
 sed -e "s/:6-x86_64/:6-i386/" \
     -e "s/libc6-dev-i386/libc6-dev-amd64/" \
     -e "s/Architecture.X86_64/Architecture.X86/" \
+    -e "s/x86_64-linux-gnu/i686-linux-gnu/" \
     -e "s/appimagetool-x86_64/appimagetool-i686/" \
     docker/Dockerfile > docker/Dockerfile.i686
 docker build -t linux-portable-i686 -f docker/Dockerfile.i686 $BUILD_ARGS docker
