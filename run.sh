596f3c7d4e6a8e4f800161d32a187a8d5a7e8119
Javier Celaya <javier.celaya@flexvdi.com>
2016-12-21 11:00:41 +0100

    Set docker container name

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ arch=${1:-x86_64}
 docker run -it --rm \
     --privileged \
     -h squeeze-$arch \
+    --name squeeze-$arch \
     -v /home/javi/src/linux-portable/src:/home/javi/src \
     -v /home/javi/build/linux-portable/${arch}-build:/home/javi/build \
     -v /home/javi/build/linux-portable/${arch}-target:/usr/local \
