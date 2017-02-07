08e94307f20bb491aedaa8bf0b4148aa90bd7bec
Javier Celaya <javier.celaya@flexvdi.com>
2017-02-07 12:20:52 +0100

    recipes/spice-gtk: Build with VA support on va variant

--- cerbero/config.py
+++ cerbero/config.py
@@ -49,7 +49,7 @@ License = enums.License
 class Variants(object):
 
     __disabled_variants = ['x11', 'alsa', 'pulse', 'cdparanoia', 'v4l2', 'sdl',
-                           'gi', 'python3', 'gtk3']
+                           'gi', 'python3', 'gtk3', 'va']
     __enabled_variants = ['debug', 'clutter', 'python', 'testspackage']
 
     def __init__(self, variants):
