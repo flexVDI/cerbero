d6ba491780e24f8d2359d50e62f6cafe8b451256
Javier Celaya <javier.celaya@flexvdi.com>
2017-08-01 14:15:47 +0200

    Add MacOS Sierra

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -96,6 +96,7 @@ class DistroVersion:
     OS_X_MOUNTAIN_LION = 'osx_mountain_lion'
     OS_X_YOSEMITE = 'osx_yosemite'
     OS_X_EL_CAPITAN = 'osx_el_capitan'
+    OS_X_SIERRA = 'osx_sierra'
     IOS_6_0 = 'ios_6_0'
     IOS_6_1 = 'ios_6_1'
     IOS_7_0 = 'ios_7_0'
